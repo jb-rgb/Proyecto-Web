f934304159a1c677915530060a8b453ecb9c18db
Jorge Barahona <bacj010509@gs.utm.mx>
2023-03-26 10:54:19 -0600

    Ya muestra advertencias cuando no cumple con las normas de la contraseña y cuando no son iguales

--- cliente/src/app/components/recuperar/recuperar.component.ts
+++ cliente/src/app/components/recuperar/recuperar.component.ts
@@ -13,6 +13,10 @@ import Swal from 'sweetalert2';
   styleUrls: ['./recuperar.component.css']
 })
 export class RecuperarComponent {
+  passwordValidationMessage = '';
+  showPasswordValidationMessage = false;
+  passwordEqualMessage = '';
+  showPasswordEqual = false;
   passwordNueva: any
   passwordCompara: any
   token: any
@@ -48,21 +52,58 @@ export class RecuperarComponent {
         this.router.navigate(['login'])
       }
       else {
-        Swal.fire({
-          position: "center",
-          icon: "error",
-          title: "La contraseña no cumple con las condiciones establecidas",
-          showConfirmButton: true
-        })
+        // Swal.fire({
+        //   position: "center",
+        //   icon: "error",
+        //   title: "La contraseña no cumple con las condiciones establecidas",
+        //   showConfirmButton: true
+        // })
       }
     }
     else {
-      Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Las contraseñas no coinciden",
-        showConfirmButton: true
-      })
+      // Swal.fire({
+      //   position: "center",
+      //   icon: "error",
+      //   title: "Las contraseñas no coinciden",
+      //   showConfirmButton: true
+      // })
     }
+  }
+  onPasswordChange(event: any) {
+    const password = event.target.value;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[@$!%?&#<>~^]/.test(password);
+
+    let message = '';
+    if(password.length > 0) {
+      if(!hasUpperCase) {
+        message = 'La contraseña debe de tener al menos una letra mayúscula. ';
+      } else if(!hasLowerCase) {
+        message = 'La contraseña debe de tener al menos una letra minúscula. ';
+      } else if(!hasNumber) {
+        message = 'La contraseña debe de tener al menos un número. ';
+      } else if(!hasSpecialChar) {
+        message = 'La contraseña debe de tener al menos un carácter especial. ';
+      }
+      this.showPasswordValidationMessage = true;
+    } else {
+      this.showPasswordValidationMessage = false;
+    }
+    this.passwordValidationMessage = message;
+  }
+  passwordEqual(event: any) {
+    const password = event.target.value;
+    let message = '';
+    if(password.length > 0) {
+      if(this.cliente1.password != this.cliente2.password) {
+        message = 'Las contraseñas no son iguales. ';
+      }
+      this.showPasswordEqual = true;
+    } else {
+      this.showPasswordEqual = false;
+    }
+    this.passwordEqualMessage = message;
   }
 }
